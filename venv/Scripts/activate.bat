4a658479d9f909bde6dc9aea4464e22a86e520a8
miloss11 <parppex@gmail.com>
2025-08-07 17:39:27 +0200

    Add full dashboard and fix plot warnings

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\0911d\Documents\sleep-health-analysis\venv
+set VIRTUAL_ENV=C:\Users\0911d\Documents\sleep-health-lifestyle-analysis\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
